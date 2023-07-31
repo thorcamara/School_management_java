5637a17234a7e9221750041f607ccb3488622c1f
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-30 23:39:03 -0300

    Added setGrade and updateFeesPaid method.

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -8,11 +8,19 @@ public class Student {
     private int feesPaid;
     private int feesTotal;
 
-    public Student(int id, String name, int grade, int feesPaid, int feesTotal) {
+    public Student(int id, String name, int grade) {
         this.id = id;
         this.name = name;
         this.grade = grade;
         this.feesPaid = feesPaid=0;
         this.feesTotal = feesTotal=3000;
+    }
+
+    public void setGrade(int grade) {
+        this.grade=grade;
+    }
+
+    public void updateFeesPaid(int fees) {
+        feesPaid += fees;
     }
 }
