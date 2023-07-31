711fa91a162e3d502aa38eec17ddc491b2ab8502
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-30 23:34:30 -0300

    Defined variables.

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -2,4 +2,10 @@ package school.management.system;
 
 public class Student {
 
+    private int id;
+    private String name;
+    private int grade;
+    private int feesPaid;
+    private int feesTotal;
+
 }
