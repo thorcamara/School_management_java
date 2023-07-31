cfed124031776a93bb6c2d35252a23014f66b964
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-30 23:35:59 -0300

    Added constructor.

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -8,4 +8,11 @@ public class Student {
     private int feesPaid;
     private int feesTotal;
 
+    public Student(int id, String name, int grade, int feesPaid, int feesTotal) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+        this.feesPaid = feesPaid=0;
+        this.feesTotal = feesTotal=3000;
+    }
 }
