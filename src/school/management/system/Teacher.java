850bde2fe5d371275d117355ed801b23da77fe2e
thorcamara <thorcamaraalthoff@gmail.com>
2023-08-02 21:44:03 -0300

    Added Teacher constructor

--- src/school/management/system/Teacher.java
+++ src/school/management/system/Teacher.java
@@ -5,4 +5,10 @@ public class Teacher {
     private int id;
     private String name;
     private int salary;
+
+    public Teacher(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+    }
 }
